b83b34f0b6157d2dd92f0d93a48a2545270b1bd1
Favour <dave.400g@gmail.com>
2024-11-28 13:50:05 +0100

    Updated the env file

--- nodeserver.js
+++ nodeserver.js
@@ -47,7 +47,7 @@ let connectedUsers = {};
 const db = mysql.createConnection({
   host: process.env.DB_HOST, // Use environment variable for host
   user: process.env.DB_USER, // Use environment variable for user
-  password: process.env.DB_PASSWORD, // Use environment variable for password
+  password: process.env.DB_PASS, // Use environment variable for password
   database: process.env.DB_NAME // Use environment variable for database name
 });
 
